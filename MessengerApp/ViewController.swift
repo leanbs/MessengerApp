b98eebaa80f7ca58e5784e21c77c55beade4d7dd
leanbs <davin.yu1110@gmail.com>
2019-07-23 20:17:56 +0700

    Add TabController

--- MessengerApp/ViewController.swift
+++ MessengerApp/ViewController.swift
@@ -6,15 +6,28 @@
 //  Copyright © 2019 Leanbs. All rights reserved.
 //
 
-import UIKit
+import AsyncDisplayKit
 
-class ViewController: UIViewController {
-
+class ViewController: ASTabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
+        self.tabBar.isTranslucent = false
+        self.tabBar.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+//        self.tabBar.tintColor = UIColor(red:0.01, green:0.67, blue:0.05, alpha:1.0)
+        
+        viewControllers = [
+            createTabItem(viewController: ASViewController(), title: "Chat"),
+            createTabItem(viewController: ASViewController(), title: "Contact"),
+            createTabItem(viewController: ASViewController(), title: "Discover"),
+        ]
     }
-
-
+    
+    
+    fileprivate func createTabItem(viewController: UIViewController, title: String) -> UIViewController {
+        viewController.view.backgroundColor = .white
+        viewController.tabBarItem.title = title
+        
+        return viewController
+    }
 }
-
